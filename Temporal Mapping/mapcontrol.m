253ed2c8b1b3877cdaf89aefe710afd5dceba5a0
Leo Sutherby <leo.sutherby24@imperial.ac.uk>
2026-02-20 17:43:33 +0000

    vector oh yeah

--- Temporal Mapping/mapcontrol.m
+++ Temporal Mapping/mapcontrol.m
@@ -1,20 +1,21 @@
 function [u_mapped] = mapcontrol(u_hf, u_lf, dt_hf, dt_lf, horizon_hf, horizon_lf, dt_output, t0_hf)
+%designed to take a row array of control signals, each a column vector 
 
-outputtime = (t0_hf:dt_output:(t0_hf+horizon_hf))';
+outputtime = (t0_hf:dt_output:(t0_hf+horizon_hf));
 
 %hf:
 lowerhf_indexes = floor((outputtime - t0_hf)./dt_hf) + 1; %indexes of output points corresponding to hf point before them
-u_mapped_hf = u_hf(lowerhf_indexes);
+u_mapped_hf = u_hf(:,lowerhf_indexes);
 
 %lf:
-t_lf = (0:dt_lf:horizon_lf)';
+t_lf = (0:dt_lf:horizon_lf);
 
 lowerlf_indexes = floor((outputtime)./dt_lf) + 1;
-u_k = u_lf(lowerlf_indexes);
+u_k = u_lf(:,lowerlf_indexes);
 t_k = t_lf(lowerlf_indexes);
 
 upperlf_indexes = ceil((outputtime)./dt_lf) + 1;
-u_kplus1 = u_lf(upperlf_indexes);
+u_kplus1 = u_lf(:,upperlf_indexes);
 t_kplus1 = t_lf(upperlf_indexes);
 
 u_mapped_lf = u_k + ((u_kplus1-u_k)./(t_kplus1 - t_k)).*(outputtime - t_k);
