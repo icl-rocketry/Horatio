e66b7cb82b5a0ad4fc7a1e20b3349b013577d25f
Leo Sutherby <leo.sutherby24@imperial.ac.uk>
2026-02-21 13:26:52 +0000

    thinking outside of the box my g

--- SCP_MPC/inequality_constraint_fn.m
+++ SCP_MPC/inequality_constraint_fn.m
@@ -1,5 +1,17 @@
 function [g] = inequality_constraint_fn()
 % Defines safety region for the rocket and will return a positive value if violated.
-    g = 0; % need to define safety region
+
+%assume rectangle bound, axes aligned around launchpad
+
+%passed in params:
+%boundaries: [x1,x2,y1,y2]
+%tightening parameters: [d1, d2, d3, d4];
+%current position [x,y]
+
+g = [x - x2 + d1;
+    x1 - x + d2;
+    y - y2 + d3;
+    y1 - y + d4];
+
 end
 
